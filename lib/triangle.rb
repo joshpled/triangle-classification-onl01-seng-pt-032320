47e7f0350066afedeb9ec792c08ff40e143bbc4c
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-01 19:13:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ attr_accessor :side_a, :side_b, :side_c,:equilateral,:isosceles, :scalene
           return :scalene if @side_a != @side_b && @side_b != @side_c && @side_c != @side_a
     end
 end
-end 
+end
 
 
 
