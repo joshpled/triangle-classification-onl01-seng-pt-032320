5c609f3315e682d1a4cd181a5296ead0ac5614e1
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-01 19:13:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ attr_accessor :side_a, :side_b, :side_c,:equilateral,:isosceles, :scalene
   end
 
   def kind
-    if @side_a + @side_b > @side_c && @side_a + @side_c > @side_b && @side_b + @side_c > @side_a && @side_a > 0 && @side_b > 0 && @side_c > 0
+    sides = [@side_a,@side_b,@side_c].sort
           return :equilateral if @side_a == @side_b && @side_b == @side_c
           return :isosceles if @side_a == @side_b && @side_b != @side_c || @side_b == @side_c && @side_a != @side_c || @side_a == @side_c && @side_b != @side_c
           return :scalene if @side_a != @side_b && @side_b != @side_c && @side_c != @side_a
