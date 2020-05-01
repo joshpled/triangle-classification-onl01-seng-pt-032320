8c629a97d76f1cf1026a52002942ca6b57cc2488
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-01 18:59:39 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -14,28 +14,5 @@ attr_accessor :side_a, :side_b, :side_c,:equilateral,:isosceles, :scalene
           return :isosceles if @side_a == @side_b && @side_b != @side_c || @side_b == @side_c && @side_a != @side_c || @side_a == @side_c && @side_b != @side_c
           return :scalene if @side_a != @side_b && @side_b != @side_c && @side_c != @side_a
     end
+
 end
-end 
-
-
-
-  # def kind
-  #   sides = [@side_a,@side_b,@side_c].sort
-  #   raise TriangleError if sides.any? {|s| s <= 0} || (sides[0]+ sides[1]) <= sides[2]
-  #     case sides.uniq.size
-  #     when 1
-  #       :equilateral
-  #     when 2
-  #       :isosceles
-  #     else
-  #       :scalene
-  #     end
-  #   end
-  #
-  #
-  # class TriangleError < StandardError
-  #   def message
-  #     "This is not a triangle!"
-  #   end
-  # end
-# end
