b8e7f6d668a7390822fee668012ab73702c808d5
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-01 19:48:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,7 @@ attr_accessor :side_a, :side_b, :side_c,:equilateral,:isosceles, :scalene
 
   def kind
     sides = [@side_a,@side_b,@side_c].uniq
+    if sides.each {|i| i <=0 }
           return :equilateral if sides.length == 1
           return :isosceles if sides.length == 2
           return :scalene if sides.length == 3
