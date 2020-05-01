5c1f47b753401f1bac76a6fcd860d408c3750104
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-01 20:27:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,44 +1,31 @@
 class Triangle
 
 attr_accessor :side_a, :side_b, :side_c,:equilateral,:isosceles, :scalene
-  # write code here
-  def initialize(side_a,side_b,side_c)
-    @side_a = side_a
-    @side_b = side_b
-    @side_c = side_c
+  
+	def initialize(*args)
+    @sides = *args.sort
   end
 
   def kind
-    sides = [@side_a,@side_b,@side_c].uniq
-    raise TriangleError if sides.any? {|s| s <= 0} || (sides[0]+ sides[1]) <= sides[2]
-        return :equilateral if sides.length == 1
-        return :isosceles if sides.length == 2
-        return :scalene if sides.length == 3
-      end
+    
+		raise TriangleError if @sides.any? {|s| s <= 0} || @sides[0]+ @sides[1] <= @sides[2]
+    
+		case @sides.uniq.size
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+    else
+      :scalene
+    end
   end
+end
 
-    class TriangleError < StandardError
-      def message
+class TriangleError < StandardError
+     
+		 def message
         "This is not a triangle!"
       end
 
 end
-
-
-
-  # def kind
-  #   sides = [@side_a,@side_b,@side_c].sort
-  #   raise TriangleError if sides.any? {|s| s <= 0} || (sides[0]+ sides[1]) <= sides[2]
-  #     case sides.uniq.size
-  #     when 1
-  #       :equilateral
-  #     when 2
-  #       :isosceles
-  #     else
-  #       :scalene
-  #     end
-  #   end
-  #
-  #
-
-# end
+end 
