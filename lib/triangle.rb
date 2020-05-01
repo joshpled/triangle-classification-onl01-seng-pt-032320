9e246a2eee1175566b0c7458f63a1f083fa10407
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-05-01 20:27:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,15 @@
 class Triangle
 
 attr_accessor :side_a, :side_b, :side_c,:equilateral,:isosceles, :scalene
-  
+
 	def initialize(*args)
     @sides = *args.sort
   end
 
   def kind
-    
+
 		raise TriangleError if @sides.any? {|s| s <= 0} || @sides[0]+ @sides[1] <= @sides[2]
-    
+
 		case @sides.uniq.size
     when 1
       :equilateral
@@ -22,10 +22,10 @@ attr_accessor :side_a, :side_b, :side_c,:equilateral,:isosceles, :scalene
 end
 
 class TriangleError < StandardError
-     
+
 		 def message
         "This is not a triangle!"
       end
 
 end
-end 
+end
